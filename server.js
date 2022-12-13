84a072bb4d521335778e877c380536230abb09ab
Rajat Sharma <36663311+iamRajatSharma@users.noreply.github.com>
2022-12-13 23:33:12 +0530

    admin panel

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const PORT = 12345
-// const ejs = require("ejs")
+
 
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
@@ -16,6 +16,10 @@ app.get("/contact", (req, res) => {
 
 app.get("/login", (req, res) => {
     res.render("login")
+})
+
+app.post("/login1", (req, res) => {
+    res.render(req.body)
 })
 
 app.get("/courses", (req, res) => {
@@ -42,6 +46,10 @@ app.get("/membership", (req, res) => {
     res.render("membership")
 })
 
+app.get("/search", (req, res) => {
+    res.render("search")
+})
+
 // ++++++++++++++++  ADMIN  ++++++++++++++++
 
 app.get("/admin/", (req, res) => {
@@ -58,6 +66,39 @@ app.get("/admin/register", (req, res) => {
 
 app.get("/admin/forgot", (req, res) => {
     res.render("admin/forgot")
+})
+
+app.get("/admin/table", (req, res) => {
+    res.render("admin/table")
+})
+
+app.get("/admin/add_category", (req, res) => {
+    res.render("admin/add_category")
+})
+
+app.get("/admin/category_list", (req, res) => {
+    res.render("admin/category_list")
+})
+
+app.get("/admin/add_product", (req, res) => {
+    res.render("admin/add_product")
+})
+
+app.get("/admin/product_list", (req, res) => {
+    res.render("admin/product_list")
+})
+
+
+app.get("/admin/order_successfull", (req, res) => {
+    res.render("admin/order_success")
+})
+
+app.get("/admin/order_failed", (req, res) => {
+    res.render("admin/order_failed")
+})
+
+app.get("/admin/form", (req, res) => {
+    res.render("admin/form")
 })
 
 // display error page
